b311bac2475724fdeb31dfccbb3eb5ae7ac3ed85
Anderson Meireles <anderson_meireles@id.uff.br>
2022-11-18 20:54:14 -0300

    ex 3 e ex 5

--- postgres.sql
+++ postgres.sql
@@ -18,7 +18,48 @@ END $$;
 SELECT indexname, tablename FROM pg_indexes;
 CALL drop_indexes_from('Track');
 
-/* 3. TO DO */
+/* 3. Query que retorna todas as foreign keys do schema, com nome da tabela e coluna */
+SELECT ccu.constraint_name AS foreign_key, tc.table_name AS tabela, ccu.column_name AS coluna FROM information_schema.constraint_column_usage ccu RIGHT JOIN information_schema.table_constraints tc ON tc.constraint_name = ccu.constraint_name WHERE (ccu.table_schema = current_schema AND constraint_type = 'FOREIGN KEY');
 
-SELECT * FROM information_schema.table_constraints tc  WHERE table_schema = current_schema AND constraint_type = 'FOREIGN KEY';
+/* 4. TO DO */
+
+/* 5. Maquina de Estados da coluna status da tabela Track */
+ALTER TABLE "Track" ADD status VARCHAR;
+
+CREATE OR REPLACE TRIGGER 
+track_state_transition
+BEFORE INSERT OR UPDATE OF status ON "Track"
+FOR EACH ROW
+EXECUTE PROCEDURE check_state_transition();
+
+
+CREATE OR REPLACE FUNCTION check_state_transition() RETURNS trigger as $check_state_transition$
+BEGIN
+	IF OLD.status = NEW.status THEN
+		RAISE EXCEPTION 'No transition needed' USING HINT = format('State already is %s.', OLD.status);
+	END IF;
+	IF OLD.status = 'created' THEN
+		IF NEW.status = 'approved' THEN
+			RAISE EXCEPTION 'Unauthorized transition' USING HINT = 'Cant transition from created to approved.';
+		ELSIF NEW.status <> 'in_analysis' THEN
+			RAISE EXCEPTION 'Unauthorized transition' USING HINT = 'This state does not exist.';
+		END IF;
+	ELSIF OLD.status = 'approved' THEN
+		IF NEW.status = 'created' THEN
+			RAISE EXCEPTION 'Unauthorized transition' USING HINT = 'Cant transition from approved to created.';
+		ELSIF NEW.status <> 'in_analysis' THEN
+			RAISE EXCEPTION 'Unauthorized transition' USING HINT = 'This state does not exist.';
+		END IF;
+	ELSIF OLD.status = 'in_analysis' THEN
+		IF NEW.status = 'created' THEN
+			RAISE EXCEPTION 'Unauthorized transition' USING HINT = 'Cant transition from in_analysis to created.';
+		ELSIF NEW.status <> 'approved' THEN
+			RAISE EXCEPTION 'Unauthorized transition' USING HINT = 'This state does not exist.';
+		END IF;
+	END IF;
+  	RETURN NEW;
+END;
+$check_state_transition$ LANGUAGE plpgsql;
+
+
 
