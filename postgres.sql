cf6c6374626b034075f8ec5e64e40b9ed1bf4a85
Anderson Meireles <anderson_meireles@id.uff.br>
2022-11-23 22:16:12 -0300

    done

--- postgres.sql
+++ postgres.sql
@@ -32,11 +32,14 @@ BEGIN
 END
 $$;
 
+-- Para verificar os indices
 SELECT
     indexname,
     tablename
 FROM
-    pg_indexes;
+    pg_indexes
+WHERE
+    SCHEMANAME = 'public';
 
 CALL drop_indexes_from ('Track');
 
@@ -219,7 +222,7 @@ CALL clone_schema ('public');
 -- substituir nome do seu schema
 /* 5. Maquina de Estados da coluna status da tabela Track */
 ALTER TABLE "Track"
-    ADD status VARCHAR(20);
+    ADD status VARCHAR(20) DEFAULT "created";
 
 CREATE OR REPLACE TRIGGER track_state_transition BEFORE INSERT
     OR UPDATE OF status ON "Track" FOR EACH ROW EXECUTE PROCEDURE check_state_transition ();
@@ -264,10 +267,16 @@ LANGUAGE plpgsql;
 
 
 /* PARTE 2 */
-/* 1. TO DO */
+/* 1. A regra semântica definida está envolvida com a máquina de estados definida na parte 1.
+ * As tracks precisam estar no status approved para serem adicionadas à uma InvoiceLine.
+ * Tracks com status created ou in_analysis devem levantar o erro ao tentar adicionar uma InvoiceLine
+ * que a referencie.
+ * Dessa forma, é necessário que a coluna status tenha sido adicionada à tabela Track.
+ * A linha que faz essa inclusão na tabela está algumas linhas de código acima.
+ */
 /* 2. Trigger que garante que uma track que não esteja Approved não pode ser adicionada à uma invoice */
 CREATE OR REPLACE TRIGGER track_state_check BEFORE INSERT
-    OR UPDATE ON "InvoiceLine" FOR EACH ROW EXECUTE PROCEDURE check_track_state ();
+    OR UPDATE OF "TrackId" ON "InvoiceLine" FOR EACH ROW EXECUTE PROCEDURE check_track_state ();
 
 CREATE OR REPLACE FUNCTION check_track_state ()
     RETURNS TRIGGER
@@ -298,60 +307,71 @@ SET
 WHERE
     "TrackId" = 1;
 
-INSERT INTO "InvoiceLine" ("InvoiceId", "TrackId", "UnitPrice", "Quantity")
-    VALUES (1, 1, 0.99, 1);
+INSERT INTO "InvoiceLine" ("InvoiceLineId", "InvoiceId", "TrackId", "UnitPrice", "Quantity")
+    VALUES (2450, 1, 1, 0.99, 1);
 
 -- Erro, track não pode ser adicionada pois não está approved
 UPDATE
     "Track"
 SET
-    status = 'created'
+    status = 'in_analysis'
 WHERE
     "TrackId" = 2;
 
-INSERT INTO "InvoiceLine" ("InvoiceId", "TrackId", "UnitPrice", "Quantity")
-    VALUES (1, 2, 0.99, 1);
+INSERT INTO "InvoiceLine" ("InvoiceLineId", "InvoiceId", "TrackId", "UnitPrice", "Quantity")
+    VALUES (2451, 1, 2, 0.99, 1);
 
 
 /* 3. */
-CREATE USER questao3;
+CREATE USER questao3 WITH PASSWORD 'your_password';
 
-GRANT EXECUTE ON procedure add_track_to_invoice TO questao3;
+GRANT CONNECT ON DATABASE postgres TO questao3;
 
-CREATE OR REPLACE procedure add_track_to_invoice(invoice_id integer, track_id integer, unit_price decimal, quantity integer)
-language plpgsql
-as $$
-declare
+CREATE OR REPLACE PROCEDURE add_track_to_invoice (invoice_id integer, track_id integer, unit_price decimal, quantity integer)
+LANGUAGE plpgsql
+SECURITY DEFINER
+AS $$
+DECLARE
     track_status "Track".status%TYPE;
     invoice_line_id integer;
-begin
-
-     SELECT
+BEGIN
+    SELECT
         status INTO track_status
     FROM
         "Track" t
     WHERE
         "TrackId" = track_id;
     IF track_status <> 'approved' THEN
-        RAISE EXCEPTION 'This track cannot be added to an invoice' USING HINT = format('Cannot add a track which state is %s', track_status);
-    END IF;
-        
-    invoice_line_id := (SELECT "InvoiceLineId"  from "InvoiceLine" il  order by  IL."InvoiceLineId" desc limit 1);
-	INSERT INTO "InvoiceLine" ("InvoiceLineId", "InvoiceId", "TrackId", "UnitPrice", "Quantity") VALUES (invoice_line_id + 1, invoice_id, track_id, unit_price, quantity);
-end 
+        RAISE EXCEPTION 'This track cannot be added to an invoice'
+            USING HINT = format('Cannot add a track which state is %s', track_status);
+        END IF;
+        invoice_line_id := (
+            SELECT
+                "InvoiceLineId"
+            FROM
+                "InvoiceLine" il
+            ORDER BY
+                IL."InvoiceLineId" DESC
+            LIMIT 1);
+        INSERT INTO "InvoiceLine" ("InvoiceLineId", "InvoiceId", "TrackId", "UnitPrice", "Quantity")
+            VALUES (invoice_line_id + 1, invoice_id, track_id, unit_price, quantity);
+END
 $$;
 
--- Funciona (Deve ser rodado com usuario com permissão)
+GRANT EXECUTE ON PROCEDURE add_track_to_invoice TO questao3;
+
+-- Funciona (Deve ser rodado com usuario com permissao)
 UPDATE
     "Track"
 SET
     status = 'approved'
 WHERE
     "TrackId" = 1;
-   
-CALL add_track_to_invoice(1, 1, 0.99, 1);
 
--- Erro, track não pode ser adicionada pois não está approved(Deve ser rodado com usuario com permissão)
+-- Deve ser rodado com usuario questao3
+CALL add_track_to_invoice (1, 1, 0.99, 1);
+
+-- Erro (Deve ser rodado com usuario com permissao)
 UPDATE
     "Track"
 SET
@@ -359,4 +379,164 @@ SET
 WHERE
     "TrackId" = 2;
 
-CALL add_track_to_invoice(1, 2, 0.99, 1)
+-- Deve ser rodado com usuario questao3
+CALL add_track_to_invoice (1, 2, 0.99, 1)
+/* 4. Valida se a coluna Total do Invoice está condizente com as InvoiceLines */
+CREATE OR REPLACE FUNCTION check_total_integrity ()
+    RETURNS TRIGGER
+    AS $check_total_integrity$
+DECLARE
+    total numeric;
+BEGIN
+    SELECT
+        SUM("UnitPrice" * "Quantity") INTO total
+    FROM
+        "InvoiceLine"
+    WHERE
+        "InvoiceId" = NEW."InvoiceId";
+    IF total <> NEW."Total" THEN
+        RAISE EXCEPTION 'Invoice Total must represent InvoiceLines totals'
+            USING HINT = format('InvoiceLines total sum is %s', total::varchar);
+        END IF;
+        RETURN NEW;
+    END;
+$check_total_integrity$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER track_invoice_total BEFORE INSERT
+    OR UPDATE OF "Total" ON "Invoice" FOR EACH ROW
+    WHEN (
+pg_trigger_depth() < 1)
+        EXECUTE PROCEDURE check_total_integrity ();
+
+-- Levanta o erro já que InvoiceLine não soma 3
+UPDATE
+    "Invoice"
+SET
+    "Total" = 3.00
+WHERE
+    "InvoiceId" = 1;
+
+CREATE OR REPLACE FUNCTION update_invoice_total ()
+    RETURNS TRIGGER
+    AS $update_invoice_total$
+DECLARE
+    total numeric;
+BEGIN
+    SELECT
+        SUM("UnitPrice" * "Quantity") INTO total
+    FROM
+        "InvoiceLine"
+    WHERE
+        "InvoiceId" = NEW."InvoiceId";
+    UPDATE
+        "Invoice"
+    SET
+        "Total" = total
+    WHERE
+        "InvoiceId" = NEW."InvoiceId";
+    RETURN NEW;
+END;
+$update_invoice_total$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER track_invoice_lines AFTER INSERT
+    OR UPDATE OF "Quantity", "UnitPrice" ON "InvoiceLine" FOR EACH ROW EXECUTE PROCEDURE update_invoice_total ();
+
+SELECT
+    "Total"
+FROM
+    "Invoice" i
+WHERE
+    "InvoiceId" = 1;
+
+UPDATE
+    "InvoiceLine"
+SET
+    "Quantity" = 2
+WHERE
+    "InvoiceId" = 1;
+
+SELECT
+    *
+FROM
+    "InvoiceLine" il
+WHERE
+    "InvoiceId" = 1;
+
+
+/* 5. Cria uma tabela com o numero de sequencia, e cria também uma tabela para testes */
+CREATE TABLE seq_numbers (
+    number int NOT NULL,
+    CONSTRAINT "PK_number" PRIMARY KEY (number)
+);
+
+INSERT INTO seq_numbers (number)
+    VALUES (1);
+
+-- Impedir que adicione mais linhas na tabela de sequência
+CREATE OR REPLACE FUNCTION limit_row ()
+    RETURNS TRIGGER
+    AS $$
+BEGIN
+    RAISE EXCEPTION 'Cannot insert to this table';
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER tr_limit_seq_numbers_row
+    BEFORE INSERT ON seq_numbers
+    FOR EACH ROW
+    EXECUTE PROCEDURE limit_row ();
+
+-- Tabela que será usada pra testes
+CREATE TABLE test_case (
+    id int NOT NULL,
+    test_message varchar(30),
+    CONSTRAINT "PK_test_case_id" PRIMARY KEY (id)
+);
+
+
+/* Tentar iniciar a transação em duas sessões simultâneas:
+ * A segunda sessão irá aguardar a primeira fazer o commit já que o SELECT FOR UPDATE
+ * foi utilizado pra pegar o número atual da sequência. Cada sessão irá fazer um INSERT
+ * na tabela de teste passando o número como coluna id (PK) e após o INSERT liberar
+ * a tabela de sequência pra próxima sessão.
+ */
+DO $$
+DECLARE
+    number_fetchd integer;
+BEGIN
+    SELECT
+        number INTO number_fetchd
+    FROM
+        SEQ_NUMBERS SN
+    FOR UPDATE;
+    INSERT INTO TEST_CASE (id, test_message)
+        VALUES (number_fetchd, 'testando!');
+    UPDATE
+        SEQ_NUMBERS
+    SET
+        number = number_fetchd + 1;
+    -- RAISE EXCEPTION 'descomente para ver o erro acontecer e não afetar a tabela';
+EXCEPTION
+    WHEN OTHERS THEN
+        UPDATE
+            SEQ_NUMBERS
+        SET
+            number = number_fetchd;
+    RAISE NOTICE 'Ocorreu um erro, valor da sequência inalterado.';
+END
+$$;
+
+-- para testes
+SELECT
+    *
+FROM
+    test_case;
+
+SELECT
+    *
+FROM
+    seq_numbers;
+
